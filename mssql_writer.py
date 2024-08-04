599e3c54529a5675c6a888484fd5ea1e101b1e36
Amin Jafari <amincorleone81@gmail.com>
2024-08-04 15:51:12 +0330

    initial

--- mssql_writer.py
+++ mssql_writer.py
@@ -33,18 +33,19 @@ def test_mssql_connection():
             'UID=sa;'
             f'PWD={db_config["PASS"]};'
         )
-        logging.info("Successfully connected to sql server.")
+        logging.info("Successfully connected to SQL Server.")
         cur = conn.cursor()
         cur.execute("CREATE DATABASE test;")
         conn.commit()
+
         cur.execute("USE test;")
+        cur.execute("CREATE TABLE hashes (id INT IDENTITY(1,1) PRIMARY KEY, hash varchar(MAX), created_at DATETIME);")
         conn.commit()
-        cur.execute("CREATE TABLE hashes (id serial PRIMARY KEY, hash TEXT, created_at TIMESTAMP);")
-        conn.commit()
+
         conn.close()
         return True
     except Exception as e:
-        logging.error(f"sql server connection failed: {e}")
+        logging.error(f"SQL Server connection failed: {e}")
         return False
 
 
@@ -58,7 +59,6 @@ def mssql_write_hash(size: int = 100) -> bool:
         )
         cur = conn.cursor()
         cur.execute("USE test;")
-        conn.commit()
         for _ in range(size):
             cur.execute(
                 f"INSERT INTO hashes (hash, created_at) VALUES ('{generate_random_hash(db_config['HASH_SIZE'])}', '{datetime.now()}')")
