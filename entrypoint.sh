59f61cb5051070a65d2841b37ef37e9110dcb645
Amin Jafari <amincorleone81@gmail.com>
2024-08-04 15:10:31 +0330

    initial

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Run odbcinst -j to verify ODBC installation
 odbcinst -j
 
+# Debug: Display the content of odbcinst.ini
+cat /etc/odbcinst.ini
+
 # Run the Python application
 exec python /home/app/mssql_writer.py
